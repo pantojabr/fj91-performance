9c6d637e2a2b99ffb2520e858393ffb96d393540
Rodrigo Caneppele <r.caneppele@gmail.com>
2017-11-16 14:10:11 -0200

    otimizando consulta dos ultimos lancamentos na home

--- src/main/java/br/com/caelum/fj91/performance/controllers/HomeController.java
+++ src/main/java/br/com/caelum/fj91/performance/controllers/HomeController.java
@@ -1,18 +1,16 @@
 package br.com.caelum.fj91.performance.controllers;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import br.com.caelum.fj91.performance.models.Curso;
 import br.com.caelum.fj91.performance.repositories.CursoRepository;
 
 @Controller
 public class HomeController {
 	
+	private static final PageRequest DEZ_ULTIMOS = new PageRequest(0, 10);
 	private final CursoRepository cursoRepository;
 
 	public HomeController(CursoRepository cursoRepository) {
@@ -21,10 +19,7 @@ public class HomeController {
 
 	@GetMapping({"/", "/home"})
 	public String index(Model model) {
-		List<Curso> cursos = cursoRepository.findAllByOrderByDataDeLancamentoDesc();
-		List<Curso> ultimos = cursos.stream().limit(10).collect(Collectors.toList());
-		
-		model.addAttribute("ultimosCursos", ultimos);
+		model.addAttribute("ultimosCursos", cursoRepository.ultimosLancamentos(DEZ_ULTIMOS));
 		return "home/index";
 	}
 	
--- src/main/java/br/com/caelum/fj91/performance/repositories/CursoRepository.java
+++ src/main/java/br/com/caelum/fj91/performance/repositories/CursoRepository.java
@@ -2,7 +2,9 @@ package br.com.caelum.fj91.performance.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.caelum.fj91.performance.models.Curso;
@@ -10,6 +12,7 @@ import br.com.caelum.fj91.performance.models.Curso;
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 	
-	List<Curso> findAllByOrderByDataDeLancamentoDesc();
+	@Query("SELECT DISTINCT(c) FROM Curso c JOIN FETCH c.categoria JOIN FETCH c.tags ORDER BY c.dataDeLancamento DESC")
+	List<Curso> ultimosLancamentos(Pageable limite);
 
 }
