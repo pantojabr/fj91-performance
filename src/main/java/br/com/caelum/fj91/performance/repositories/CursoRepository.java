fb993810c88a3b47bc757b0936bf9bda761dac93
Rodrigo Caneppele <r.caneppele@gmail.com>
2017-11-16 14:15:52 -0200

    otimizando consulta da pagina de listagem de cursos

--- src/main/java/br/com/caelum/fj91/performance/repositories/CursoRepository.java
+++ src/main/java/br/com/caelum/fj91/performance/repositories/CursoRepository.java
@@ -12,7 +12,10 @@ import br.com.caelum.fj91.performance.models.Curso;
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 	
-	@Query("SELECT DISTINCT(c) FROM Curso c JOIN FETCH c.categoria JOIN FETCH c.tags ORDER BY c.dataDeLancamento DESC")
+	@Query("SELECT DISTINCT(c) FROM Curso c JOIN FETCH c.categoria JOIN FETCH c.instrutor LEFT JOIN FETCH c.tags")
+	List<Curso> findAll();
+
+	@Query("SELECT DISTINCT(c) FROM Curso c JOIN FETCH c.categoria LEFT JOIN FETCH c.tags ORDER BY c.dataDeLancamento DESC")
 	List<Curso> ultimosLancamentos(Pageable limite);
 
 }
